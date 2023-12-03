935149b161e37f680d7535f01899d793220b0b1e
Bey9434 <aluca9434@gmail.com>
2023-12-04 02:48:13 +0900

    test

---
+++ Assets/scripts/buildscript.cs
@@ -0,0 +1,16 @@
+using UnityEditor;
+
+public class BuildScript
+{
+    public static void BuildGame()
+
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scenes/MainScene.unity" };
+        buildPlayerOptions.locationPathName = "builds/wordle.exe";
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64; // 例：Windows 64ビット
+        buildPlayerOptions.options = BuildOptions.None;
+
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+    }
+}
